a9b801a7a080a34c427d4a1d73ab8c28b82b010a
RedEmber-hub <redember38@gmail.com>
2026-06-02 11:22:44 +0300

    refactoring code and edit styles

--- src/components/ThirdSlide/ThirdSlide.tsx
+++ src/components/ThirdSlide/ThirdSlide.tsx
@@ -6,33 +6,35 @@ export default function ThirdSlide() {
   return (
     <Slide>
       <div className="thirdSlide">
-        <div className="thirdSlide__image-background">
-          <img src="/img/slide3.jpg" />
+        <div className="thirdSlide__bg">
+          <img src="/img/slide3.jpg" alt="" />
         </div>
 
-        <div className="thirdSlide__text-block">
-          <div className="thirdSlide__title">
-            <h1>
-              Для ценителей комфорта
-              <br /> и качества в уходе за волосами
-            </h1>
+        <div className="thirdSlide__content">
+          <div className="thirdSlide__text-block">
+            <div className="thirdSlide__title">
+              <h1>
+                Для ценителей комфорта
+                <br /> и качества в уходе за волосами
+              </h1>
+            </div>
+
+            <div className="thirdSlide__subtitle">
+              <p>Бесщеточный мотор BLDC</p>
+            </div>
           </div>
 
-          <div className="thirdSlide__subtitle">
-            <p>Бесщеточный мотор BLDC</p>
-          </div>
-        </div>
+          <div className="thirdSlide__sticker">
+            <div className="thirdSlide__sticker-icon">
+              <img src="/icon/star.svg" alt="" />
+            </div>
 
-        <div className="thirdSlide__sticker">
-          <div className="thirdSlide__sticker-icon">
-            <img src="/icon/star.svg" />
-          </div>
-
-          <div className="thirdSlide__sticker-text">
-            <p>
-              Салонная <br />
-              мощность
-            </p>
+            <div className="thirdSlide__sticker-text">
+              <p>
+                Салонная <br />
+                мощность
+              </p>
+            </div>
           </div>
         </div>
       </div>
